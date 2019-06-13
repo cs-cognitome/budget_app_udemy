a569dd2cc7f0cf6d6477f7ef5b63f0d204a1f420
cs-cognitome <gaya.scienza.v@gmail.com>
2019-06-13 11:01:48 +0300

    insert html in the DOM

--- budget/app.js
+++ budget/app.js
@@ -34,6 +34,7 @@ var budgetController = (function() {
             // unique number for each item 
             // ID = last ID + 1
             // new ID
+            // if array is empty, the ID === 0
             if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
@@ -69,7 +70,9 @@ var UIController = (function(){
         inputType: '.add__type', 
         inputDescription: '.add__description', 
         inputValue: '.add__value', 
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn', 
+        incomeContainer: '.income__list', 
+        expensesContainer: '.expenses__list'
     }
 
     return { 
@@ -80,6 +83,31 @@ var UIController = (function(){
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         }, 
+
+        addListItem: function(obj, type) { 
+            // create html string with placeholder text 
+            var html, newHtml, element; 
+
+            if(type === 'inc') {
+                element = DOMstrings.incomeContainer; 
+
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer; 
+
+                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+
+            // replace the placeholder with actual text with id method
+            newHtml = html.replace('%id%', obj.id); 
+            newHtml = newHtml.replace('%description%', obj.desctiption); 
+            newHtml = newHtml.replace('%value%', obj.value); 
+       
+            // insert the html in the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml); 
+        },
+
         getDOMstrings: function() { 
             return DOMstrings; 
         }
